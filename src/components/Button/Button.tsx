fe1a99a97c3df76088646ebc66648a5301c3fafa
Dmitry <gix73@yandex.ru>
2023-05-23 16:10:02 +0300

    feat: test styled-components

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,18 @@
 import React, { type FC } from "react";
 import "./button.css"; // 👈new addition
+import { ButtonS } from "./styled";
 
 export interface ButtonProps {
   label: string;
   handleClick: () => void;
 }
 const Button: FC<ButtonProps> = ({ label, handleClick }: ButtonProps) => (
-  <button type="button" className="btn" onClick={handleClick}>
+  <ButtonS type="button" onClick={handleClick}>
     {label}
-  </button>
+  </ButtonS>
+  // <button type="button" className="btn" onClick={handleClick}>
+  //   {label}
+  // </button>
 );
 
 export default Button;
