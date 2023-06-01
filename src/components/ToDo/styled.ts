17c11d60bb35d31cfb7b0e3d1b6cabb9912d8ed9
Dmitry <gix73@yandex.ru>
2023-06-01 21:53:47 +0300

    feat: add todo with localstorage

--- src/components/ToDo/styled.ts
+++ src/components/ToDo/styled.ts
@@ -14,3 +14,25 @@ export const ToDoWrapper = styled.div`
   top: 0;
   left: 0;
 `;
+export const CloseBtn = styled.div`
+  position: absolute;
+  cursor: pointer;
+  right: 10px;
+  bottom: 5px;
+  z-index: 999;
+  position: absolute;
+  cursor: pointer;
+  font-size: 11px;
+`;
+
+export const ListWrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  margin-top: 10px;
+  width: 100%;
+  flex-grow: 1;
+  overflow-y: auto;
+  margin-bottom: 20px;
+  gap: 10px;
+`;
